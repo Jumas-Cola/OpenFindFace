f7743c7f7651e148a01103a9fe85299019769bd7
Jumas <kbbyfl91@gmail.com>
2023-11-19 12:22:05 +0300

    Добавлен вывод всех полей записи

--- vue/src/components/face/FaceCardItem.vue
+++ vue/src/components/face/FaceCardItem.vue
@@ -9,22 +9,42 @@ const props = defineProps({
     required: true
   }
 });
+
+const getFaceDataProps = () => {
+  return Object.keys(props.faceData).filter((key) => !['image', 'id'].includes(key));
+};
+
+const checkIsUrl = (url: string) => {
+  if (!url) {
+    return false;
+  }
+  return `${url}`.startsWith('http://') || `${url}`.startsWith('https://');
+};
 </script>
 
 <template>
-  <hr>
-  <table class="table">
+  <hr />
+  <table class="table table-bordered">
     <tbody>
-      <tr>
+      <tr v-if="props.faceData.image">
         <th class="fw-bold" scope="row">{{ t('photo') }}</th>
         <td class="d-flex justify-content-end">
           <img :src="`/${props.faceData.image}`" width="100" height="100" />
         </td>
       </tr>
-      <tr>
-        <th class="fw-bold" scope="row">{{ t('name') }}</th>
-        <td class="text-end">{{ props.faceData.name }}</td>
-      </tr>
+      <template v-for="key in getFaceDataProps()" :key="key">
+        <tr>
+          <th class="fw-bold" scope="row">{{ t(key) }}</th>
+          <td class="text-end">
+            <a v-if="checkIsUrl(props.faceData[key])" :href="props.faceData[key]" target="_blank">
+              {{ props.faceData[key] }}
+            </a>
+            <span v-else>
+              {{ props.faceData[key] }}
+            </span>
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
